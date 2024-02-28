5027e282c425c8abad27e9e3c87709246ee3d519
Batuto Joshua <147791934+batutojoshua@users.noreply.github.com>
2024-02-28 14:07:00 +0800

    Add files via upload

---
+++ lab-2/Home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="Stylesheet.css">
+</head>
+<body>    
+    <div class="container">  
+        <form action="Loginform.php" method="POST">
+        <h2>Login Successfully</h2>
+            <button value="button" name="button">Logout</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ lab-2/db_conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$sname= "localhost";
+$uname= "root";
+$password = "";
+$db_name = "ipt101_db";
+
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
