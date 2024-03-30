18b6a3f61e4439971d2e68be30eea175e9655c02
Batuto Joshua <147791934+batutojoshua@users.noreply.github.com>
2024-03-31 01:43:41 +0800

    Update verifyemail.php

--- lab-3/verifyemail.php
+++ lab-3/verifyemail.php
@@ -60,7 +60,7 @@ if(isset($_POST['verify'])){
     }
 }
 ?>
-<!DOCTYPE html>+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -69,23 +69,52 @@ if(isset($_POST['verify'])){
     <!-- Add Bootstrap CSS link -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        html, body {
-            height: 100%;
-        } 
-        input {
-            width: 100px;
-            height: 40px;
-            margin-bottom: 2px;
-        }
-    </style>
+    body {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #f8f9fa;
+    }
+    .container {
+        width: 30%;
+      background-color: #fff;
+      padding: 15px;
+      border-radius: 5px;
+      box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+    }
+    h2 {
+      margin-bottom: 20px;
+    }
+    input[type="text"] {
+      width: 40%;
+      padding: 10px;
+      margin-bottom: 20px;
+      border: 1px solid #ced4da;
+      border-radius: 5px;
+      box-sizing: border-box;
+    }
+    button[type="submit"] {
+      padding: 10px 20px;
+      background-color: #228B22;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+    button[type="submit"]:hover {
+      background-color: #0056b3;
+    }
+  </style>
 </head>
-<body class="d-flex justify-content-center align-items-center">
-    <div class="container text-center">
-        <form action="" method="post">
-            <h2>Verify Your account using the OTP</h2><br>
-            <input  type="text" name="otp"><br>
-            <button type="submit" name="verify" class="btn btn-primary">Verify</button>
-        </form>
-    </div>
+<body>
+  <div class="container text-center justify-content-center">
+    <form action="" method="post">
+      <h2>Verifying your account</h2>
+      <input type="text" name="otp" placeholder="Enter OTP">
+      <div class="pt-2"></div>
+      <button type="submit" name="verify">Verify</button>
+    </form>
+  </div>
 </body>
 </html>
